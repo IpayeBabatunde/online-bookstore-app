0877380fd73fbb4944c599913c8cca8821c96467
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-07 03:45:02 +0100

    init commit

---
+++ src/main/java/com/ipaye/onlinebookstoreapp/OnlineBookstoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.onlinebookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookstoreAppApplication.class, args);
+    }
+
+}
